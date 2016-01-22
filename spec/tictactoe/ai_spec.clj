f923048906d9efb5f2648b455a2b4102618df17b
Cat McLoughlin <catmclough@gmail.com>
2016-01-22 14:24:27 -0800

    Further tests for minimax algorithm

--- spec/tictactoe/ai_spec.clj
+++ spec/tictactoe/ai_spec.clj
@@ -4,18 +4,22 @@
 
 (describe "choose-move"
   (it "chooses to win the game when a possible move leads to direct victory"
-      (def board [0 "X" 2
-                  3 "O" "X"
-                 "X" 7 "O"])
+      (def board [0 "X" 2 3 "O" "X" "X" 7 "O"])
       (should= "0" (choose-move board)))
 
-  (it "always chooses the winning spot"
+  (it "chooses the winning spot when not the first available index"
       (def board [0  "O" "X" "X" 4 "O" "X" "O" "X"])
       (should= "4" (choose-move board)))
 
   (it "returns the best move when it means a direct block of the opponent's chance to win"
     (def board ["X" "X" 2 "O" 4 5 "X" 7 "O"])
-    (should= "2" (choose-move board))))
+    (should= "2" (choose-move board)))
+
+(it "returns the best move when it means a direct block of the opponent's win and that spot is not the first available spot"
+    (def board ["X" "O" 2 "O" 4 5 "X" 7 "X"])
+    (should= "4" (choose-move board))))
+
+
 
 (describe "score-game"
     (it "returns 1 if the ai has won the given board state"
