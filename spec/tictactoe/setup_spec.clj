052d60ccb43dfe2c3ecc20d214c0e73aa31925d7
Cat McLoughlin <catmclough@gmail.com>
2016-02-10 14:19:03 -0800

    Clean up setup spec

--- spec/tictactoe/setup_spec.clj
+++ spec/tictactoe/setup_spec.clj
@@ -5,37 +5,46 @@
 
 (def board-size 9)
 
-(describe "board"
-	(with empty-board [0 1 2 3 4 5 6 7 8])
+(def invalid-game-type-num 999)
+
+(def invalid-game-type-choice "chutes and ladders")
+
+(def empty-board [0 1 2 3 4 5 6 7 8])
+
+(describe "setup"
   (tags :setup-spec)
 
-(describe "valid-type-choice?"
-  (it "returns true if given game type choice is valid option"
-    (should= true (valid-type-choice? player-ai-game-type))))
+  (describe "set-game-type"
+		(around [it]
+		  (with-out-str (it)))
 
-(describe "set-game-type"
-  (around [it]
-    (with-out-str (it)))
+		(it "returns the type choice if valid"
+		  (should= two-ai-game-type (set-game-type two-ai-game-type)))
 
-  (it "returns the type choice if valid"
-    (with-redefs
-      [console/get-game-type (fn [] 2)]
-        (should= 2 (set-game-type)))))
-
-(describe "setup-game"
-  (around [it]
-    (with-out-str
+		(it "prompts the player to choose a different type if given a number that does not correspond to a game type"
       (with-redefs
-        [welcome-players (fn [] nil)
-         set-game-type (fn [] 2)]
-        (it))))
+		    [console/get-game-type (fn [] two-ai-game-type)]
+		    (should= two-ai-game-type (set-game-type invalid-game-type-num))))
 
-  (it "welcomes the players"
-    (should-invoke welcome-players {:times 1} (setup-game)))
+      (it "prompts the player to choose a different type if given a string as the game-type choice"
+      (with-redefs
+		    [console/get-game-type (fn [] player-ai-game-type)]
+        (should= player-ai-game-type (set-game-type invalid-game-type-choice)))))
 
-  (it "prompts the user to choose a game type"
-    (should-invoke console/game-type-prompt {:times 1} (setup-game)))
+  (describe "setup-game"
+	 (around [it]
+	   (with-out-str
+	     (with-redefs
+	       [welcome-players (fn [] nil)
+          console/get-game-type (fn [] 2)]
+	       (it))))
 
-  (it "tells describes the chosen game type to the user"
-    (should-invoke console/two-ai-game-description {:times 1} (setup-game)))))
+	 (it "welcomes the players"
+	   (should-invoke welcome-players {:times 1} (setup-game)))
+
+	 (it "prompts the user to choose a game type"
+	   (should-invoke console/game-type-prompt {:times 1} (setup-game)))
+
+	 (it "describes the chosen game type to the user"
+	   (should-invoke console/two-ai-game-description {:times 1} (setup-game)))))
 
