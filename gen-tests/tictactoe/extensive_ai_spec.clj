bfa28630d59fc294f7121f800e271ccbdc820d37
Cat McLoughlin <catmclough@gmail.com>
2016-02-10 13:33:26 -0800

    Fix generative tests to cover all possible player moves vs. AI

--- gen-tests/tictactoe/extensive_ai_spec.clj
+++ gen-tests/tictactoe/extensive_ai_spec.clj
@@ -7,45 +7,26 @@
 
 (def ai-player "O")
 
-(defn player-can-win?
-  ([board]
-    (player-can-win? board (board/available-spaces board)))
-  ([board spots-to-check]
-    (if (empty? spots-to-check)
-      false
-      (if (= human-player (board/winner (board/fill-space (first spots-to-check) human-player board)))
-        true
-        (recur board (rest spots-to-check))))))
+(defn get-hypothetical-boards [board]
+  (let [open-spots (board/available-spaces board)]
+    (map #(board/fill-space % human-player board) open-spots)))
 
-(defn choose-best-player-move
-  ([board]
-    (choose-best-player-move board (board/available-spaces board)))
-  ([board open-spots]
-    (if (= 1 (count open-spots))
-      (first open-spots)
-      (if (= ai-player (board/winner (board/fill-space (first open-spots) ai-player board)))
-        (first open-spots)
-        (recur board (rest open-spots))))))
-
-(defn ai-did-not-lose? [board]
-  (if (or (= (board/winner board) ai-player) (board/cats-game? board))
-    true
-    false))
-
-(defn ai-cannot-lose?
+(defn possible-winners
   ([]
-    (ai-cannot-lose? (vec (range 9))))
-  ([board]
-    (if (board/game-over? board)
-      (ai-did-not-lose? board)
-      (if (= (board/active-player board) ai-player)
-        (recur (board/fill-space (ai/choose-move board ai-player) ai-player board))
-        (if (= true (player-can-win? board))
-          false
-          (recur (board/fill-space (choose-best-player-move board) human-player board)))))))
+    (possible-winners (board/make-board) []))
+  ([board possible-winner-list]
+      (if (board/game-over? board)
+        (conj possible-winner-list (board/winner board))
+        (if (= (board/active-player board) ai-player)
+          (recur (board/fill-space (ai/choose-move board ai-player) ai-player board) possible-winner-list)
+            (map #(possible-winners % possible-winner-list) (get-hypothetical-boards board))))))
 
 (describe "Generate Many AI Board Outcomes"
   (tags :gen)
   (it "the AI never loses when using the 'choose-move' algorithm"
-      (should= true (ai-cannot-lose?))))
+      (println "Generating All Possible Winners: ")
+      (let [winners (flatten (possible-winners))]
+        (apply println winners)
+        (println (str (count winners) " possible outcomes found"))
+        (should= nil (some #(= human-player %) winners)))))
 
